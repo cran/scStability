1265bfca5e9f39d25b86a85cd3652aa19c5d05da
Ben Abrahams <ben.abrahams.de@gmail.com>
2025-06-17 06:50:06 +0000

    version 1.0.0

---
+++ tests/testthat/test-compareEmb.R
@@ -0,0 +1,9 @@
+test_that("All embeddings are compared pairwise", {
+  sce <- scRNAseq::ZeiselBrainData()
+  counts_matrix <- SummarizedExperiment::assay(sce, "counts")
+  counts_matrix <- as.matrix(counts_matrix[1:500, 1:50])
+  emb_list <- scStability::createEmb(counts_matrix, n_runs = 5, n_cores = 5)
+  result <- scStability::compareEmb(emb_list)
+  expect_equal(dim(result$all_pairwise_correlations), c(5, 5))
+})
+
